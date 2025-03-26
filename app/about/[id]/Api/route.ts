4ba70723f5806042f07289dc1e173e1af34e4a53
jason <jasonchinonso20@gmail.com>
2025-03-26 05:47:33 +0100

    Modified

--- app/about/[id]/Api/route.ts
+++ app/about/[id]/Api/route.ts
@@ -5,8 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
 
     try {
-
-    
+       
         const findIndex = tableData.findIndex(item => item.id === parseInt(params.id));
 
         return NextResponse.json(tableData[findIndex], { status: 201 });
