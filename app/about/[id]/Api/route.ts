050b1f093fd4c9516c0b19fa1a6f7203058c9d63
jason <jasonchinonso20@gmail.com>
2025-03-27 21:38:43 +0100

    Modified

--- app/about/[id]/Api/route.ts
+++ app/about/[id]/Api/route.ts
@@ -21,6 +21,23 @@ export async function PATCH(req: NextRequest, { params }: { params: { id: string
 
     }
 
+}
 
+export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+    try {
+        const findIndex = tableData.findIndex(item => item.id === parseInt(params.id));
+        tableData.splice(findIndex, 1);
+        
+        return NextResponse.json(tableData, { status: 201 });
 
+    } catch (error) {
+        return NextResponse.json(
+            {
+                error: "Internal Server Error",
+                status: 500,
+            },
+            { status: 500 }
+        );
+
+    }
 }
--- app/about/[id]/page.tsx
+++ app/about/[id]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>hi page</div>
+    <div>hi pagee</div>
   )
 }
 
